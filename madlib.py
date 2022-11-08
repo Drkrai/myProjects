ea727c4daa09bc3413fab8fd36cdff12debeab16
Drkrai <114841316+Drkrai@users.noreply.github.com>
2022-11-08 12:44:12 +0700

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,20 @@
+adjective = input("Enter adjective ").upper()
+adjective2 = input("Enter adjective ").upper()
+verb = input("Enter verb ").upper()
+verb2 = input("Enter verb ").upper()
+madlib=f"""
+An old silent {adjective}...
+A frog jumps into the pond,
+{verb}! Silence again.
+
+Autumn moonlight-
+a worm {verb2} {adjective2}
+into the chestnut.
+
+In the twilight rain
+these brilliant-hued hibiscus -
+A lovely sunset.
+"""
+print(madlib)
+
+
